8d2741e4c1adeb25a1c24c722736ec3d48470546
Catherine Bronzo <cbronzo@mac.com>
2021-02-07 03:13:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+let main = document.querySelector('#main');
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'John Million is the champion';
+
+document.appendChild(newHeader);
